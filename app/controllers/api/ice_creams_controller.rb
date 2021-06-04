36ea62b487ea1c302cebbcd5fd831fd46ba78525
Alex Racinski <aracinski2@gmail.com>
2021-06-04 16:55:58 -0500

    add ice creams index action

--- app/controllers/api/ice_creams_controller.rb
+++ app/controllers/api/ice_creams_controller.rb
@@ -1,2 +1,7 @@
 class Api::IceCreamsController < ApplicationController
+  def index
+    @ice_creams = IceCream.all
+    render 'index.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/ice_creams" => "ice_creams#index"
+  end
 end
